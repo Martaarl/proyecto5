938f013b0942bdebbe4a2e8476bf781090e51093
Martaarl <marali23@gmail.com>
2025-08-18 16:59:17 +0200

    ya le he dado funcionalidad y he corregido los errores que me surgían. También he añadido la puntuación. Solo me queda maquetar

--- src/components/main/whoIsWho/whoIsWho.js
+++ src/components/main/whoIsWho/whoIsWho.js
@@ -1,4 +1,4 @@
-import whoIsCategories from '../../../utils/whoIsWho/whoIsWhoUtils';
+import whoIsCategories, { increaseScore, initScore, resetScoreWho } from '../../../utils/whoIsWho/whoIsWhoUtils';
 import './whoIsWho.css'
 
 export default function createWhoIsWho () {
@@ -20,31 +20,33 @@ export default function createWhoIsWho () {
         { nombre: 'Nadia',piel: 'oscura', cabello: 'negro rizado', gafas: true, camiseta: 'roja', accesorio: null, image: '/public/assets/characters/9.png' },
         { nombre: 'Diego',piel: 'clara', cabello: 'calvo con barba', gafas: false, camiseta: 'azul', accesorio: null, image: '/public/assets/characters/10.png' },
 
-        { nombre: 'Clara',piel: 'clara', cabello: 'gris corto', gafas: false, camiseta: 'roja', accesorio: null, image: '/public/assets/characters/11.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'marrón corto con barba', gafas: false, camiseta: 'azul', accesorio: null, image: '/public/assets/characters/12.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'negro liso', gafas: true, camiseta: 'turquesa', accesorio: null , image: '/public/assets/characters/13.png'},
-        { nombre: 'Clara',piel: 'clara', cabello: 'pelirrojo corto con gorro rojo', gafas: false, camiseta: 'verde', accesorio: 'gorro', image: '/public/assets/characters/14.png'},
-        { nombre: 'Clara',piel: 'clara', cabello: 'marrón corto con barba', gafas: false, camiseta: 'azul', accesorio: null, image: '/public/assets/characters/15.png' },
+        { nombre: 'Ana',piel: 'clara', cabello: 'gris corto', gafas: false, camiseta: 'roja', accesorio: null, image: '/public/assets/characters/11.png' },
+        { nombre: 'Santi',piel: 'clara', cabello: 'marrón corto con barba', gafas: false, camiseta: 'azul', accesorio: null, image: '/public/assets/characters/12.png' },
+        { nombre: 'Paqui',piel: 'clara', cabello: 'negro liso', gafas: true, camiseta: 'turquesa', accesorio: null , image: '/public/assets/characters/13.png'},
+        { nombre: 'Manuel',piel: 'clara', cabello: 'pelirrojo corto con gorro rojo', gafas: false, camiseta: 'verde', accesorio: 'gorro', image: '/public/assets/characters/14.png'},
+        { nombre: 'Ricardo',piel: 'clara', cabello: 'marrón corto con barba', gafas: false, camiseta: 'azul', accesorio: null, image: '/public/assets/characters/15.png' },
 
-        { nombre: 'Clara',piel: 'oscura', cabello: 'negro liso largo', gafas: false, camiseta: 'roja', accesorio: null, image: '/public/assets/characters/16.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'pelirrojo corto', gafas: false, camiseta: 'verde', accesorio: null, image: '/public/assets/characters/17.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'gris corto', gafas: true, camiseta: 'roja', accesorio: null, image: '/public/assets/characters/18.png'},
-        { nombre: 'Clara',piel: 'clara', cabello: 'rubio liso', gafas: true, camiseta: 'negra', accesorio: null, image: '/public/assets/characters/19.png'},
-        { nombre: 'Clara',piel: 'clara', cabello: 'pelirrojo con barba', gafas: false, camiseta: 'azul', accesorio: null, image: '/public/assets/characters/20.png' },
+        { nombre: 'Alba',piel: 'oscura', cabello: 'negro liso largo', gafas: false, camiseta: 'roja', accesorio: null, image: '/public/assets/characters/16.png' },
+        { nombre: 'Juan',piel: 'clara', cabello: 'pelirrojo corto', gafas: false, camiseta: 'verde', accesorio: null, image: '/public/assets/characters/17.png' },
+        { nombre: 'Esmeralda',piel: 'clara', cabello: 'gris corto', gafas: true, camiseta: 'roja', accesorio: null, image: '/public/assets/characters/18.png'},
+        { nombre: 'Toni',piel: 'clara', cabello: 'rubio liso', gafas: true, camiseta: 'negra', accesorio: null, image: '/public/assets/characters/19.png'},
+        { nombre: 'Flora',piel: 'clara', cabello: 'pelirrojo con barba', gafas: false, camiseta: 'azul', accesorio: null, image: '/public/assets/characters/20.png' },
 
-        { nombre: 'Clara',piel: 'clara', cabello: 'pelirrojo con barba', gafas: false, camiseta: 'negra', accesorio: null, image: '/public/assets/characters/21.png'},
-        { nombre: 'Clara',piel: 'clara', cabello: 'marrón corto con gorro rojo', gafas: true, camiseta: 'azul', accesorio: 'gorro', image: '/public/assets/characters/22.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'rubio liso', gafas: true, camiseta: 'negra', accesorio: null , image: '/public/assets/characters/23.png'},
-        { nombre: 'Clara',piel: 'clara', cabello: 'marrón corto', gafas: false, camiseta: 'verde', accesorio: null, image: '/public/assets/characters/24.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'pelirrojo corto con gafas', gafas: true, camiseta: 'amarilla', accesorio: null, image: '/public/assets/characters/25.png' },
+        { nombre: 'Neha',piel: 'clara', cabello: 'pelirrojo con barba', gafas: false, camiseta: 'negra', accesorio: null, image: '/public/assets/characters/21.png'},
+        { nombre: 'Ron',piel: 'clara', cabello: 'marrón corto con gorro rojo', gafas: true, camiseta: 'azul', accesorio: 'gorro', image: '/public/assets/characters/22.png' },
+        { nombre: 'Teresa',piel: 'clara', cabello: 'rubio liso', gafas: true, camiseta: 'negra', accesorio: null , image: '/public/assets/characters/23.png'},
+        { nombre: 'Susana',piel: 'clara', cabello: 'marrón corto', gafas: false, camiseta: 'verde', accesorio: null, image: '/public/assets/characters/24.png' },
+        { nombre: 'Rubén',piel: 'clara', cabello: 'pelirrojo corto con gafas', gafas: true, camiseta: 'amarilla', accesorio: null, image: '/public/assets/characters/25.png' },
 
-        { nombre: 'Clara',piel: 'oscura', cabello: 'negro liso largo', gafas: false, camiseta: null, accesorio: null, image: '/public/assets/characters/26.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'pelirrojo corto', gafas: false, camiseta: 'verde', accesorio: null, image: '/public/assets/characters/27.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'gris corto', gafas: true, camiseta: 'roja', accesorio: null , image: '/public/assets/characters/28.png'},
-        { nombre: 'Clara',piel: 'clara', cabello: 'rubio liso', gafas: true, camiseta: 'negra', accesorio: null, image: '/public/assets/characters/29.png' },
-        { nombre: 'Clara',piel: 'clara', cabello: 'pelirrojo con barba', gafas: false, camiseta: 'azul', accesorio: null , image: '/public/assets/characters/30.png'}
+        { nombre: 'Kim',piel: 'oscura', cabello: 'negro liso largo', gafas: false, camiseta: null, accesorio: null, image: '/public/assets/characters/26.png' },
+        { nombre: 'Rita',piel: 'clara', cabello: 'pelirrojo corto', gafas: false, camiseta: 'verde', accesorio: null, image: '/public/assets/characters/27.png' },
+        { nombre: 'Natalia',piel: 'clara', cabello: 'gris corto', gafas: true, camiseta: 'roja', accesorio: null , image: '/public/assets/characters/28.png'},
+        { nombre: 'Fran',piel: 'clara', cabello: 'rubio liso', gafas: true, camiseta: 'negra', accesorio: null, image: '/public/assets/characters/29.png' },
+        { nombre: 'Flora',piel: 'clara', cabello: 'pelirrojo con barba', gafas: false, camiseta: 'azul', accesorio: null , image: '/public/assets/characters/30.png'}
       
     ];
+
+    let charPlaying;
 
     const whoIs = document.createElement('div');
     whoIs.className = 'whoIsDiv';
@@ -59,7 +61,21 @@ export default function createWhoIsWho () {
     const answerDiv = document.createElement('div');
     answerDiv.className = 'answerDiv';
 
-    whoIs.append(player, whoIsButton, answerDiv);
+    const possibleName = document.createElement('div');
+    possibleName.textContent = '¿Quién crees que soy?';
+    possibleName.className = 'solution';
+        
+        const solutionText = document.createElement('input');
+        solutionText.className = 'solutionText';
+        solutionText.type = 'text';
+        solutionText.textContent = '¿Quién crees que soy?';
+
+        const solutionButton = document.createElement('button');
+        solutionButton.className = 'solutionButton';
+
+    possibleName.append(solutionText, solutionButton);
+
+    whoIs.append(player, whoIsButton,possibleName, answerDiv);
 
     const whoIsCharacters = document.createElement('div');
     whoIsCharacters.className ='whoIsCharacters';
@@ -68,30 +84,69 @@ export default function createWhoIsWho () {
     whoIsQuestions.className = 'whoIsQuestions';
 
     whoIsButton.addEventListener('click', () => {
+            answerDiv.textContent = '';
+            solutionText.value = '';
+            attempts = 2;
+            whoIsCharacters.style.display = 'flex';
+            possibleName.style.display = 'block';
+            possibleName.textContent = '';
+            possibleName.append(solutionText, solutionButton);
 
-            let charPlaying = [...characters].sort(()=>Math.random()-0.5).slice(0, 1)[0];
+            whoIsScore.style.display = 'flex';
+
+            charPlaying = [...characters].sort(()=>Math.random()-0.5).slice(0, 1)[0];
             player.src = charPlaying.image;
             player.alt = charPlaying.nombre;
             player.style.display ='none';
            
+            let otherChars = characters.filter(c=> c !== charPlaying);
+            let charImg = [...otherChars].sort(()=>Math.random()-0.5).slice(0,9);
+            charImg.push(charPlaying);
+            charImg = charImg.sort(()=> Math.random()-0.5);
+
             whoIsCharacters.innerHTML = '';
 
-            let charAleatory = [...characters].sort(()=>Math.random() - 0.5).slice(0, 10);
-
-            charAleatory.forEach(character=>{
-                const charImg = document.createElement('img');
-                charImg.className = 'char';
-                charImg.src = character.image;
-                charImg.alt = character.nombre;
-                whoIsCharacters.appendChild(charImg);
+            charImg.forEach(character=>{
+                const char = document.createElement('img');
+                char.className = 'char';
+                char.src = character.image;
+                char.alt = character.nombre;
+                whoIsCharacters.appendChild(char);
             });
 
             whoIsQuestions.innerHTML = ''; 
+            whoIsCategories(whoIsQuestions, charPlaying, answerDiv, whoIsCharacters, possibleName);
+        });
 
-            whoIsCategories(whoIsQuestions, charPlaying, answerDiv);
-        })
+        let attempts = 2;
 
-    whoIsBoard.append(whoIs, whoIsCharacters);
+        solutionButton.addEventListener('click', () => {
+
+            const solution = solutionText.value.trim().toLowerCase();
+
+            if(!charPlaying){
+                return;
+            };
+            if (!solution) return;
+            if (solution === charPlaying.nombre.toLowerCase()) {
+                answerDiv.textContent = '¡HAS ACERTADO!';
+                increaseScore(50);
+                player.style.display ='flex';
+                whoIsCharacters.style.display = 'none';
+                possibleName.textContent = '';
+            } else {
+                attempts --;
+                answerDiv.textContent = '¡Prueba otra vez! Solo tienes un intento más';
+                if (attempts === 0){
+                    answerDiv.textContent = 'JUEGA OTRA VEZ';
+                    whoIsCharacters.style.display = 'none';
+                    possibleName.textContent = '';
+                }
+            }
+            
+        });
+
+    whoIsBoard.append(whoIs, whoIsCharacters, whoIsQuestions);
 
     const whoIsAside = document.createElement('aside');
     whoIsAside.className = 'whoIsAside';
@@ -99,7 +154,17 @@ export default function createWhoIsWho () {
 
     const whoIsScore = document.createElement('div');
     whoIsScore.className = 'whoIsScore';
+    whoIsScore.textContent = `Puntuación: ${initScore()}`;
 
-    section.append(whoIsBoard, whoIsAside, whoIsQuestions, whoIsScore);
+    const resetButtonWho = document.createElement('button');
+    resetButtonWho.className = 'resetButtonWho';
+    resetButtonWho.textContent = '¡Empieza de nuevo!';
+    resetButtonWho.addEventListener('click', () => {
+        resetScoreWho();
+    })
+
+    whoIsScore.appendChild(resetButtonWho);
+
+    section.append(whoIsBoard, whoIsAside, whoIsScore);
 
 } 
--- src/utils/whoIsWho/whoIsWhoUtils.js
+++ src/utils/whoIsWho/whoIsWhoUtils.js
@@ -2,7 +2,7 @@
 export default function whoIsCategories (whoIsQuestion, charPlaying, answerDiv ) {
     const categories = {
         piel: ['clara', 'oscura'],
-        cabello: ['rubio', 'negro', 'pelirrojo', 'marrón'],
+        cabello: ['rubio', 'negro', 'pelirrojo', 'marrón', 'gris'],
         gafas: [true, false],
         camiseta: ['roja', 'azul', 'verde', 'amarilla', 'negra'],
         accesorio: ['gorro', 'sombrero', null]
@@ -24,12 +24,64 @@ export default function whoIsCategories (whoIsQuestion, charPlaying, answerDiv )
     }
 }
 
-export function checkQuestions (charPlaying, category, options, answerDiv) {
+let score = 100;
+
+export function initScore(){
+    const savedScore = localStorage.getItem('whoIsScore');
+    score = savedScore ? parseInt (savedScore) : 100;
+    updateScoreWho ();
+    return score;
+}
+
+export function decreaseScore (amount) {
+    score -= amount;
+    if(score<0) score = 0;
+    localStorage.setItem('whoIsScore', score);
+    updateScoreWho();
+    return score;
+}
+
+export function increaseScore (amount) {
+    score+= amount;
+    localStorage.setItem('whoIsScore', score);
+    updateScoreWho();
+    return score;
+}
+
+export function updateScoreWho() {
+    const whoIsScoreDiv = document.querySelector('.whoIsScore');
+    
+    if(!whoIsScoreDiv) return;
+
+    const resetButtonWho = document.querySelector('.resetButtonWho');
+    whoIsScoreDiv.textContent = `Puntuación: ${score}`;
+
+    if (resetButtonWho) whoIsScoreDiv.appendChild(resetButtonWho);
+}
+
+export function resetScoreWho (){
+    score = 100;
+    localStorage.setItem('whoIsScore', score);
+    updateScoreWho();
+}
+
+export function checkQuestions (charPlaying, category, options, answerDiv, savedScore) {
+    const whoIsCharacters = document.querySelector('.whoIsCharacters');
+    const possibleName = document.querySelector('.solution');
+    const whoIsQuestions = document.querySelector('.whoIsQuestions');
+
     if (!charPlaying) return;
 
     if (charPlaying[category] === options) {
         answerDiv.textContent = '¡Si tiene esa característica!';
     } else {
         answerDiv.textContent = 'No coincide 😩';
+        decreaseScore(20);
     }
+    if(score<=0){
+        answerDiv.textContent = '¡Se acabó el juego!';
+        whoIsCharacters.style.display=' none';
+        possibleName.textContent = '';
+        whoIsQuestions.textContent = '';
+        }
 }
